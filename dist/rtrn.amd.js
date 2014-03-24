a2af480df5ac6e9020f5dfb2952d350bc3326149
Alan Plum <me@pluma.io>
2014-03-24 18:11:35 +0100

    Bump to 0.2.1

--- dist/rtrn.amd.js
+++ dist/rtrn.amd.js
@@ -1,4 +1,4 @@
-/*! rtrn 0.2.0 Original author Alan Plum <me@pluma.io>. Released into the Public Domain under the UNLICENSE. @preserve */
+/*! rtrn 0.2.1 Original author Alan Plum <me@pluma.io>. Released into the Public Domain under the UNLICENSE. @preserve */
 define(function(require, exports, module) {
 module.exports = rtrn;
 
